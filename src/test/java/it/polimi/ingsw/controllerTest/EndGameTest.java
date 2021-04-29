067251d3e9011c1b630802021b94661ab44fb246
andrea_sanchini <andrea.sanchini@mail.polimi.it>
2021-04-29 17:01:18 +0200

    new: EndGame draw test

--- src/test/java/it/polimi/ingsw/controllerTest/EndGameTest.java
+++ src/test/java/it/polimi/ingsw/controllerTest/EndGameTest.java
@@ -4,7 +4,6 @@ import it.polimi.ingsw.controller.*;
 import it.polimi.ingsw.model.*;
 import it.polimi.ingsw.model.gameboard.NumOfShelf;
 import it.polimi.ingsw.server.Server;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -54,5 +53,62 @@ public class EndGameTest {
         player1Ds.get(2).add(new DevCard(null, 3, 1, Colour.PURPLE, null, null));
         gameController.endGame();
         assertTrue(game.getWinners().size() == 1 && game.getWinners().get(0).getNickname().equals("Andri"));
+    }
+
+    @Test
+    public void DrawEndGameTest() {
+        GameController gameController = new GameController(new Server());
+        Game game = gameController.getGame();
+        Player player1 = new Player("Andri", game);
+        game.addPlayer(player1);
+        Player player2 = new Player("Desa", game);
+        game.addPlayer(player2);
+        Player player3 = new Player("Gianlu", game);
+        game.addPlayer(player3);
+        player1.getBoard().getItinerary().updatePosition(17);
+        player2.getBoard().getItinerary().updatePosition(17);
+        List<ResourcePosition> player1Warehouse = new ArrayList<>();
+        player1Warehouse.add(new ResourcePosition(1, Resource.SERVANT, Place.WAREHOUSE, NumOfShelf.ONE));
+        player1Warehouse.add(new ResourcePosition(1, Resource.COIN, Place.WAREHOUSE, NumOfShelf.TWO));
+        player1Warehouse.add(new ResourcePosition(1, Resource.COIN, Place.WAREHOUSE, NumOfShelf.TWO));
+        player1Warehouse.add(new ResourcePosition(1, Resource.SHIELD, Place.WAREHOUSE, NumOfShelf.THREE));
+        player1Warehouse.add(new ResourcePosition(1, Resource.SHIELD, Place.WAREHOUSE, NumOfShelf.THREE));
+        player1.getBoard().getWarehouse().incrementResource(player1Warehouse);
+        player2.getBoard().getWarehouse().incrementResource(player1Warehouse);
+        List<ResourcePosition> player1Chest = new ArrayList<>();
+        player1Chest.add(new ResourcePosition(3, Resource.STONE, Place.CHEST));
+        player1Chest.add(new ResourcePosition(2, Resource.SHIELD, Place.CHEST));
+        player1.getBoard().getChest().incrementResource(player1Chest);
+        player2.getBoard().getChest().incrementResource(player1Chest);
+        LeaderCard leaderCard11 = new LeaderCard(3, null, Resource.SERVANT, LeaderType.DEPOT);
+        leaderCard11.setPlayed(true);
+        player1.addFakeLeaderCard(leaderCard11);
+        player2.addFakeLeaderCard(leaderCard11);
+        LeaderCard leaderCard12 = new LeaderCard(2, null, Resource.SHIELD, LeaderType.DISCOUNT);
+        leaderCard12.setPlayed(true);
+        player1.addFakeLeaderCard(leaderCard12);
+        player2.addFakeLeaderCard(leaderCard12);
+        player1.getBoard().getItinerary().setCardStatus(CardStatus.FACE_UP, 0);
+        player1.getBoard().getItinerary().setCardStatus(CardStatus.FACE_UP, 1);
+        player1.getBoard().getItinerary().setCardStatus(CardStatus.DISCARDED, 2);
+        player2.getBoard().getItinerary().setCardStatus(CardStatus.FACE_UP, 0);
+        player2.getBoard().getItinerary().setCardStatus(CardStatus.FACE_UP, 1);
+        player2.getBoard().getItinerary().setCardStatus(CardStatus.DISCARDED, 2);
+        List<List<DevCard>> player1Ds= player1.getBoard().getDevSpace().getCards();
+        List<List<DevCard>> player2Ds= player2.getBoard().getDevSpace().getCards();
+        player1Ds.get(0).add(new DevCard(null, 1, 1, Colour.GREEN, null, null));
+        player1Ds.get(0).add(new DevCard(null, 6, 2, Colour.PURPLE, null, null));
+        player1Ds.get(1).add(new DevCard(null, 4, 1, Colour.PURPLE, null, null));
+        player1Ds.get(1).add(new DevCard(null, 5, 2, Colour.GREEN, null, null));
+        player1Ds.get(1).add(new DevCard(null, 12, 3, Colour.YELLOW, null, null));
+        player1Ds.get(2).add(new DevCard(null, 3, 1, Colour.PURPLE, null, null));
+        player2Ds.get(0).add(new DevCard(null, 1, 1, Colour.GREEN, null, null));
+        player2Ds.get(0).add(new DevCard(null, 6, 2, Colour.PURPLE, null, null));
+        player2Ds.get(1).add(new DevCard(null, 4, 1, Colour.PURPLE, null, null));
+        player2Ds.get(1).add(new DevCard(null, 5, 2, Colour.GREEN, null, null));
+        player2Ds.get(1).add(new DevCard(null, 12, 3, Colour.YELLOW, null, null));
+        player2Ds.get(2).add(new DevCard(null, 3, 1, Colour.PURPLE, null, null));
+        gameController.endGame();
+        assertTrue(game.getWinners().size() == 2 && game.getWinners().get(0).getNickname().equals("Andri") && game.getWinners().get(1).getNickname().equals("Desa"));
     }
 }
